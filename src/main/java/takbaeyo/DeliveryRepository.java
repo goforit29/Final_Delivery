43d390555f513ee10c994b2bf3c1fb1480fbcc05
goforit29 <goforit29@gmail.com>
2020-10-23 16:36:01 +0900

    first commit

---
+++ src/main/java/takbaeyo/DeliveryRepository.java
@@ -0,0 +1,8 @@
+package takbaeyo;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface DeliveryRepository extends PagingAndSortingRepository<Delivery, Long>{
+
+
+}
